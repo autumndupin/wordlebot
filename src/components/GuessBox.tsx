965a1a0eeb7ba19c8f8a29a1a72bc594f0024d4e
autumn dupin <autumndupin@gmail.com>
2024-05-25 18:43:56 -0400

    write guessbox component

---
+++ src/components/GuessBox.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+interface GuessBoxProps {
+    letter: string;
+    color: string;
+    onClick?: () => void;
+    dataTestId: string;
+}
+
+const GuessBox: React.FC<GuessBoxProps> = ({ letter, color, onClick, dataTestId }) => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '60px',
+                height: '60px',
+                border: '1px solid',
+                borderRadius: '4px',
+                bgcolor: color,
+                cursor: onClick ? 'pointer' : 'default',
+            }}
+            onClick={onClick}
+            data-testid={dataTestId}
+        >
+            <Typography variant="h5" color={color === 'white' ? 'black' : 'white'}>
+                {letter.toUpperCase()}
+            </Typography>
+        </Box>
+    );
+};
+
+export default GuessBox;
