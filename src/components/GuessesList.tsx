6309446cd6a4257bc5c34a00b657b5fcb1af25a0
autumn dupin <autumndupin@gmail.com>
2024-05-25 18:44:04 -0400

    write guesseslist component

---
+++ src/components/GuessesList.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import GuessBox from './GuessBox';
+import { WordleRequestItem } from '../api/api'; // Make sure the correct path is used
+
+interface GuessesListProps {
+    guesses: WordleRequestItem[];
+    theme: any;
+}
+
+const GuessesList: React.FC<GuessesListProps> = ({ guesses, theme }) => {
+    return (
+        <Box data-testid="guesses-container">
+            {guesses.map((item, index) => (
+                <Box key={index} marginBottom={2}>
+                    <Typography>Guess #{index + 1}: {item.word.toUpperCase()}</Typography>
+                    <Grid container justifyContent="center">
+                        {item.word.split('').map((letter, i) => (
+                            <Grid item key={i}>
+                                <GuessBox
+                                    letter={letter}
+                                    color={
+                                        item.clue[i] === 'g' ? theme.palette.green.main :
+                                        item.clue[i] === 'y' ? theme.palette.yellow.main :
+                                        'white'
+                                    }
+                                    dataTestId="guessed-letter-box"
+                                />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
+            ))}
+        </Box>
+    );
+};
+
+export default GuessesList;
